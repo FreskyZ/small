f37b47519e9f334cbeb685554d0c4a0422c1ab2f
FreskyZ <FreskyZ@outlook.com>
2023-02-01 15:51:54 +0800

    mastermind: add auto for host

--- mastermind/solve.py
+++ mastermind/solve.py
@@ -93,7 +93,7 @@ def get_info_entropy(combinations, index, debug):
             reductions += (math.log(len(combinations)) - math.log(len(nextstep))) / math.log(2)
             if debug:
                 print(f'IF GUESS {combinations[index]} AND ANSWER IS {answer} THEN REDUCE {len(combinations) - len(nextstep)}')
-    return f'{reductions / (len(combinations) - 1):.2f}'
+    return round(reductions / (len(combinations) - 1), 2)
 
 # assist gaming hosted by others
 def assist(duplicatable):
@@ -134,7 +134,7 @@ def host(duplicatable, answer):
             print(f'> p? (current possibilities)')
             print(f'> r? (recommendation)')
             print(f'> v? (advanced recommendation)')
-            print(f'> g? (current guesses)')
+            print(f'> h? (guess history)')
             print(f'> input like RGBY ({" ".join(NAMES[1:])})')
             print(f'> exit')
         elif onetry in ('a?', 'A?'):
@@ -149,38 +149,94 @@ def host(duplicatable, answer):
             print('maybe? ' + ' '.join(NAMES[c] for c in recommend))
         elif onetry in ('v?', 'V?'):
             if len(combinations) > 32 or len(combinations) == 1:
-                print('advanced recommendation not available for over 32 possiblities')
+                print('advanced recommendation not available')
             else:
+                max_entropy = 0
+                max_entropy_combinations = []
                 for i, a in enumerate(combinations):
                     entropy = get_info_entropy(combinations, i, False)
-                    print(f'[{i + 1}] ' + ' '.join(NAMES[c] for c in a) + f' {entropy}')
+                    if entropy > max_entropy:
+                        max_entropy = entropy
+                        max_entropy_combinations = [a]
+                    elif entropy == max_entropy:
+                        max_entropy_combinations.append(a)
+                    print(f'[{i + 1}] ' + ' '.join(NAMES[c] for c in a) + f' {entropy:.2f}b')
+                if len(max_entropy_combinations) < len(combinations):
+                    choice = random.choice(max_entropy_combinations)
+                    print(f'[RECOMMEND] ' + ' '.join(NAMES[c] for c in choice))
+                else:
+                    print('[NO RECOMMEND] all same entropy')
         elif onetry in ('vg?', 'VG?'):
             if len(combinations) > 32 or len(combinations) == 1:
-                print('advanced recommendation not available for over 32 possiblities')
+                print('advanced recommendation not available')
             else:
                 for i, a in enumerate(combinations):
                     entropy = get_info_entropy(combinations, i, True)
-                    print(f'[{i + 1}] ' + ' '.join(NAMES[c] for c in a) + f' {entropy}')
-        elif onetry in ('g?', 'G?'):
-            for guess, counts in guesses:
-                print(f'[{counts[0]} HIT {counts[1]} BLOW] ' + ' '.join(NAMES[c] for c in guess))
+                    print(f'[{i + 1}] ' + ' '.join(NAMES[c] for c in a) + f' {entropy:.2f}b')
+        elif onetry in ('h?', 'H?'):
+            for guess, counts, entropy in guesses:
+                print(f'[{counts[0]} HIT {counts[1]} BLOW] ' + ' '.join(NAMES[c] for c in guess) + f' {entropy:.2f}b')
         elif len(onetry) != 4 or any(c for c in onetry if c.upper() not in PARSE):
             print('invalid guess, see help')
         else:
             guess = [PARSE[c.upper()] for c in onetry]
             counts = get_count(answer, guess)
-            guesses.append((guess, counts))
+            before_entropy = math.log(len(combinations)) / math.log(2)
             combinations = [c for c in combinations if get_count(c, guess) == counts]
+            guess_entropy = before_entropy - math.log(len(combinations)) / math.log(2)
+            remaining_entropy = math.log(len(combinations)) / math.log(2)
+            guesses.append((guess, counts, guess_entropy))
             if counts[0] == 4:
-                print(f'{counts[0]} HIT {counts[1]} BLOW GAME OVER!')
+                print(f'{counts[0]} HIT {counts[1]} BLOW {guess_entropy:.2f}b GAME OVER!')
                 break
             else:
-                print(f'{counts[0]} HIT {counts[1]} BLOW')
+                print(f'{counts[0]} HIT {counts[1]} BLOW {guess_entropy:.2f}b REMAINING {remaining_entropy:.2f}b')
+
+def autoonce(duplicatable):
+    answer = generate_combination(duplicatable)
+    combinations = get_all_combinations(duplicatable)
+
+    step = 1
+    guess = None
+    while True:
+        if step == 1:
+            guess = (2, 2, 1, 1)
+        # elif step == 2:
+        #     guess = (3, 3, 4, 4)
+        elif len(combinations) == 1:
+            guess = combinations[0]
+        else:
+            max_entropy = 0
+            max_entropy_combinations = []
+            for i, a in enumerate(combinations):
+                entropy = get_info_entropy(combinations, i, False)
+                if entropy > max_entropy:
+                    max_entropy = entropy
+                    max_entropy_combinations = [a]
+                elif entropy == max_entropy:
+                    max_entropy_combinations.append(a)
+            guess = random.choice(max_entropy_combinations)
+        counts = get_count(answer, guess)
+        before_entropy = math.log(len(combinations)) / math.log(2)
+        combinations = [c for c in combinations if get_count(c, guess) == counts]
+        guess_entropy = before_entropy - math.log(len(combinations)) / math.log(2)
+        remaining_entropy = math.log(len(combinations)) / math.log(2)
+        guess_display = ' '.join(NAMES[c] for c in guess)
+        if counts[0] == 4:
+            print(f'{step}> {guess_display}')
+            print(f'   {counts[0]} HIT {counts[1]} BLOW {guess_entropy:.2f}b GAME OVER!')
+            break
+        else:
+            print(f'{step}> {guess_display}')
+            print(f'   {counts[0]} HIT {counts[1]} BLOW {guess_entropy:.2f}b -> {remaining_entropy:.2f}b')
+        step += 1
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2 and sys.argv[1] == 'assist':
         assist(len(sys.argv) == 3 and sys.argv[2] == 'dup')
     elif len(sys.argv) >= 2 and sys.argv[1] == 'host':
         host(len(sys.argv) >= 3 and sys.argv[2] == 'dup', sys.argv[3] if len(sys.argv) == 4 else None)
+    elif len(sys.argv) >= 2 and sys.argv[1] == 'auto':
+        autoonce(len(sys.argv) >= 3 and sys.argv[2] == 'dup')
     else:
         print('hab.py assist [dup] or hab.py host dup/nodup [answer]')
